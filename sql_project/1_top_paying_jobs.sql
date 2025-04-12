bad02633f8f7893e404a836da76db1b251669f87
user43553 <144586977+user43553@users.noreply.github.com>
2025-04-12 13:34:15 +0500

    just saving

---
+++ sql_project/1_top_paying_jobs.sql
@@ -0,0 +1,10 @@
+/*
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focuses on job postings with specified salaries (remove nulls).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment opportunities.
+*/
+
+SELECT *
+FROM job_postings_fact
+WHERE job_work_from_home = TRUE
